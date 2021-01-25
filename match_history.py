01813533cca79b5f3dbbcccd6324d83c8d93bf8f
Dillon Plunkett <dillonplunkett@users.noreply.github.com>
2021-01-25 17:04:15 +0000

    cut down to one API call per match in history

--- match_history.py
+++ match_history.py
@@ -1,23 +1,80 @@
+# Produce a csv of a summoner's match history with timestamp, patch, 
+# red/blue side, win/loss, match duration, player names, their champs, their roles, 
+# their KDAs, and their gold
+
 import cassiopeia as cass
 from cassiopeia import Summoner, Match
-from cassiopeia.data import Season, Queue
-from collections import Counter
+from cassiopeia.data import Season, Queue, Role
+import pandas
 
-cass.set_riot_api_key("RGAPI-00f1ddd1-d709-4272-8249-3032d2f29cb0")  # This overrides the value set in your configuration/settings.
-cass.set_default_region("NA")
-summoner = Summoner(name="DaftPunkRock", region="NA")
-match_history = summoner.match_history
-match_history(seasons={Season.season_9}, queues={Queue.normal_draft_fives})
+SUMMONER_NAME = "xctrunner"
+SUMMONER_REGION = "NA"
+N_MATCHES = 100
+
+
+def get_lane(map_id, summoner):
+    if map_id == 12:
+        return "ARAM"
+    if summoner.lane is None or (map_id != 1 and map_id != 2 and map_id != 11):
+        return None
+    if summoner.lane.value == "BOT_LANE":
+        if summoner.role == Role.duo_carry:
+            return "BOT"
+        else:
+            return "SUPPORT"
+    else:
+        return summoner.lane.value
+
+
+def make_row(match, summoner):
+    
+    me = match.participants.filter(lambda x: x.summoner == summoner)[0]
+    was_blue = me.id in [x.id for x in match.blue_team.participants]
+
+    row = {
+        "date": match.creation,
+        "patch": match.version,
+        "my_champ": champion_id_to_name_mapping[me.champion.id], 
+        "side": "BLUE" if was_blue else "RED", 
+        "win": "WIN" if was_blue == match.blue_team.win else "LOSS", 
+        "duration": match.duration
+    }
+
+    for i, participant in enumerate(match.participants):
+        pstr = f"p{i}_"
+        st = participant.stats
+        participant_data = {
+            pstr + "name": participant.summoner.name, 
+            pstr + "champ": participant.champion.name, 
+            pstr + "lane": get_lane(match.map.id, participant),
+            pstr + "kills": st.kills,
+            pstr + "deaths": st.deaths, 
+            pstr + "assists": st.assists, 
+            pstr + "gold": st.gold_earned
+        }
+        row.update(participant_data)
+    
+    return row
+
+
+with open("credentials.txt", "r") as f:
+    api_key = f.readline().strip()
+cass.set_riot_api_key(api_key)
+cass.set_default_region(SUMMONER_REGION)
 
 champion_id_to_name_mapping = {champion.id: champion.name for champion in cass.get_champions(region="NA")}
-played_champions = Counter()
-for match in match_history:
-    champion_id = match.participants[summoner.name].champion.id
-    champion_name = champion_id_to_name_mapping[champion_id]
-    played_champions[champion_name] += 1
-print("Length of match history:", len(match_history))
 
-print("Top 10 champions {} played:".format(summoner.name))
-for champion_name, count in played_champions.most_common(10):
-    print(champion_name, count)
-print()
+summoner = Summoner(name=SUMMONER_NAME, region=SUMMONER_REGION)
+match_history = summoner.match_history
+
+data_list = []
+for match in match_history[:N_MATCHES]:
+    try:    
+        row = make_row(match, summoner)
+        data_list.append(row)
+    except Exception as e:
+        print(f"Issue with match {match.id} from {match.creation}. {e}")
+        continue
+
+df = pandas.DataFrame(data=data_list)
+df.to_csv(f"{summoner.name}_match_history_{N_MATCHES}.csv", index=False)
